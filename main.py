faa54753ace8fa697c452addca4cef426a6b7b2f
Maya Kline <mkline24@biomedscienceacademy.org>
2020-12-10 17:55:29 +0000

    I added the code that makes it print out the problem and accept an answer. if the answer is wrong, it will say so and give you the correct answer. It will also give you the ammount of questions that you ask for.

--- main.py
+++ main.py
@@ -12,8 +12,38 @@ hwMany : will use this to determine how many problems to give
 
 import random
 
-level = input("What difficulty would you like?(easy, med, hard)" )
-operator = input("What type of math would you like to practice?(+,-,*) ")
+#level = input("What difficulty would you like?(easy, med, hard) " )
+operator = input("What would you like to practice?(+, -, *) ")
 hwMany = int(input("How many would you like to practice? "))
 
+if operator == "+":
+  for i in range (hwMany):
+    a1 = random.randint(1,20) #this is "a1" for add 1 and is the first random number be added
+    a2 = random.randint(1,20) #this is "a2" for add 2 and is the second random number to be added
+    prob = int(input((str(a1) + " + " + str(a2) + " = ")))
+    if prob == a1 + a2:
+      print("Correct!")
+    else:
+      print("Incorrect. The correct answer was " + str(a1 + a2) + ".")
 
+
+elif  operator == "-":
+  for i in range (hwMany):
+    a1 = random.randint(1,20) #this is "a1" for add 1 and is the first random number be added
+    a2 = random.randint(1,20) #this is "a2" for add 2 and is the second random number to be added
+    prob = int(input((str(a1) + " - " + str(a2) + " = ")))
+    if prob == a1 - a2:
+      print("Correct!")
+    else:
+      print("Incorrect. The correct answer was " + str(a1 - a2) + ".")
+
+
+elif  operator == "*":
+  for i in range (hwMany):
+    a1 = random.randint(1,20) #this is "a1" for add 1 and is the first random number be added
+    a2 = random.randint(1,20) #this is "a2" for add 2 and is the second random number to be added
+    prob = int(input((str(a1) + " * " + str(a2) + " = ")))
+    if prob == a1 * a2:
+      print("Correct!")
+    else:
+      print("Incorrect. The correct answer was " + str(a1 * a2) + ".")
