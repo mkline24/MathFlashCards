6d043f89f0017d645ec4373167b45514aae37ef3
Maya Kline <mkline24@biomedscienceacademy.org>
2020-12-14 16:52:29 +0000

    I made it so that it asks that user for a maximum and minimum for then use that to give the random integers a range. This allows the user to tailor it to their specific needs. I also added a line after certain lines to help divide it and make it seems more organized..

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ This code will make flash cards for the user to practice math based on what sett
 
 Functions:
 
-level : will store the difficulty level that is entered
+mx : will store how high the numbers should go
+
+mn: will store how low the numbers should go
 
 operator : will store whether they want to do addition, subtration, or multiplication
 
@@ -12,36 +14,42 @@ hwMany : will use this to determine how many problems to give
 
 import random
 
-#level = input("What difficulty would you like?(easy, med, hard) " )
+
 operator = input("What would you like to practice?(+, -, *) ")
 hwMany = int(input("How many would you like to practice? "))
+mx = int(input("Set the max (enter any whole number): " ))
+mn = int(input("Set the Minimum (enter any whole number): "))
+print(" ")
 
 if operator == "+":
   for i in range (hwMany):
-    a1 = random.randint(1,20) #this is "a1" for add 1 and is the first random number be added
-    a2 = random.randint(1,20) #this is "a2" for add 2 and is the second random number to be added
+    a1 = random.randint(mn,mx) #this is "a1" for add 1 and is the first random number be added
+    a2 = random.randint(mn,mx) #this is "a2" for add 2 and is the second random number to be added
     prob = int(input((str(a1) + " + " + str(a2) + " = ")))
     if prob == a1 + a2:
       print("Correct!")
+      print(" ")
     else:
       print("Incorrect. The correct answer was " + str(a1 + a2) + ".")
+      print(" ")
 
 
 elif  operator == "-":
   for i in range (hwMany):
-    a1 = random.randint(1,20) #this is "a1" for add 1 and is the first random number be added
-    a2 = random.randint(1,20) #this is "a2" for add 2 and is the second random number to be added
+    a1 = random.randint(mn,mx) #this is "a1" for add 1 and is the first random number be added
+    a2 = random.randint(mn,mx) #this is "a2" for add 2 and is the second random number to be added
     prob = int(input((str(a1) + " - " + str(a2) + " = ")))
     if prob == a1 - a2:
-      print("Correct!")
+      print("Correct! ")
     else:
-      print("Incorrect. The correct answer was " + str(a1 - a2) + ".")
+      print("Incorrect. The correct answer was " + str(a1 - a2) + ". ")
+
 
 
 elif  operator == "*":
   for i in range (hwMany):
-    a1 = random.randint(1,20) #this is "a1" for add 1 and is the first random number be added
-    a2 = random.randint(1,20) #this is "a2" for add 2 and is the second random number to be added
+    a1 = random.randint(mn,mx) #this is "a1" for add 1 and is the first random number be added
+    a2 = random.randint(mn,mx) #this is "a2" for add 2 and is the second random number to be added
     prob = int(input((str(a1) + " * " + str(a2) + " = ")))
     if prob == a1 * a2:
       print("Correct!")
